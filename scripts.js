dd6fc31324a84e2fe5de37c285a0b7d7fc359ec0
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-12-10 20:21:03 -0300

    criei as variáveis

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let gridJogador = [
+    [[], [], []],
+    [[], [], []],
+    [[], [], []]
+];
+
+let gridNPC = [
+    [[], [], []],
+    [[], [], []],
+    [[], [], []]
+];
+
+let pontuacaoJogador = [0, 0, 0];
+
+let pontuacaoNPC = [0, 0, 0];
