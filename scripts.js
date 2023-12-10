776781c725f904d14b451fe9d5b10da8be8f5dc9
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-12-10 20:26:29 -0300

    criei algumas funções básicas

--- scripts.js
+++ scripts.js
@@ -13,3 +13,79 @@ let gridNPC = [
 let pontuacaoJogador = [0, 0, 0];
 
 let pontuacaoNPC = [0, 0, 0];
+
+//FUNCOES DAQUI PARA BAIXO
+
+function preencherGrid(vetor, gridSelector) {
+    let index = 0;
+    const celula = document.querySelectorAll(`${gridSelector} .celula`)
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++){
+            celula[index].textContent = vetor[i][j];
+            index++;
+        }
+    }
+} // PREENCHE A GRID COM OS VALORES
+
+function preencherGridPontuacao(vetor, gridSelector) {
+    let index = 0;
+    const celula = document.querySelectorAll(`${gridSelector} .celulaPontuacao`)
+    for (let i = 0; i < vetor.length; i++) {
+        celula[index].textContent = vetor[i];
+        index++;
+    }
+} // PREENCHE A GRID DA PONTUAÇÃO
+
+function preencherGridComImagens(vetor, gridSelector) {
+    let index = 0;
+    const celula = document.querySelectorAll(`${gridSelector} .celula`)
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++){
+            celula[index].innerHTML = '';
+            if (vetor[i][j] >= 1 && vetor[i][j] <= 6) {
+                const imgLocal = document.createElement('img');
+                imgLocal.src = `./dado${vetor[i][j]}.svg`;
+                celula[index].appendChild(imgLocal);
+            } 
+            index++;
+        }
+    }
+} //PREENCHE A GRID COM IMAGENS
+
+function atualizaTelaComImagens() {
+    preencherGridComImagens(gridJogador, '#jogador');
+    preencherGridComImagens(gridNPC, '#npc');
+} // CHAMA AS FUNÇÇÕES DE PREENCHER GRID
+
+function attPontuacao(tipoGrid, tipoIdGrid) {
+    const celulas = document.querySelectorAll(`${tipoIdGrid} .celulaPontuacao`);
+
+    for (let i = 0; i < 3; i++){
+        let soma = 0;
+        for (let j = 0; j < 3; j++){
+            soma += tipoGrid[j][i] * contadorColuna(i, tipoGrid[j][i], tipoGrid);
+        }
+        celulas[i].textContent = soma;
+    }
+}; // ATUALIZA A PONTUACAO DE GRID PONTUACAO
+
+function contadorColuna(coluna, valor, qualGrid){
+    let vezes = 0;
+    for(let i = 0; i < 3; i++){
+        if(qualGrid[i][coluna] == valor){
+            vezes++;
+        }
+    }
+    return vezes;
+    // a contagem é feita individualmente para cada valor passado no argumento
+} // CONTA OS DADOS REPETIDOS NA COLUNAA
+
+function atualizaTela() {
+    preencherGrid(gridJogador, '#jogador');
+    preencherGrid(gridNPC, '#npc');
+    preencherGridPontuacao(pontuacaoJogador, '#pontuacaoJogador');
+    preencherGridPontuacao(pontuacaoNPC, '#pontuacaoNPC');
+    atualizaTelaComImagens();
+    attPontuacao(gridJogador, '#pontuacaoJogador');
+    attPontuacao(gridNPC, '#pontuacaoNPC');
+}
